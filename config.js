62f6e5f77af8bf3c3b0a061296d8b514d30ef57c
Insulora <Insulora@outlook.com>
2026-05-24 19:10:59 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js (このファイルは後でGitの除外対象にします)
-const CLUSTER_URL = "71e8ee3c3db84e0fbc36f7a3c23a442c.s1.eu.hivemq.cloud";
-const MQTT_USER   = "espuser";
-const MQTT_PASS   = "Exhibition2026";
+const CLUSTER_URL = "YOUR_CLUSTER_URL";
+const MQTT_USER   = "YOUR_MQTT_USER";
+const MQTT_PASS   = "YOUR_MQTT_PASS";
