f76e417e413c5287f891b1a48562a5e64a0677ec
Insulora <Insulora@outlook.com>
2026-05-24 18:52:17 +0900

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js (このファイルは後でGitの除外対象にします)
+const CLUSTER_URL = "あなたのHiveMQクラスターURL（例: xxxxx.s1.eu.hivemq.cloud）";
+const MQTT_USER   = "あなたのMQTTユーザー名";
+const MQTT_PASS   = "あなたのMQTTパスワード";
